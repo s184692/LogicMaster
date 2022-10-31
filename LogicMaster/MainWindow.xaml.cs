3cf8fed4ea8bc036eb97a17286e6a7c72a74bac1
s184692 <s184692@student.pg.edu.pl>
2022-10-31 16:21:37 +0100

    Mechanizm dostosowywania rozmiaru okna. Dodanie przycisków wyjścia i restartu

--- LogicMaster/MainWindow.xaml.cs
+++ LogicMaster/MainWindow.xaml.cs
@@ -20,9 +20,25 @@ namespace LogicMaster
     /// </summary>
     public partial class MainWindow : Window
     {
+        public double windowTargetAspectRatio;
+        public double windowInitialMinWidth;
+        public double mainGridHeightRatio;
         public MainWindow()
         {
             InitializeComponent();
+            windowTargetAspectRatio = 9.0 / 16.0;
+            windowInitialMinWidth = MinWidth;
+            mainGridHeightRatio = mainGrid.RowDefinitions[1].Height.Value / Height;
+        }
+
+        protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
+        {
+            double targetWidth = sizeInfo.NewSize.Height * windowTargetAspectRatio;
+            targetWidth = targetWidth > windowInitialMinWidth ? targetWidth : windowInitialMinWidth;
+            MinWidth = targetWidth + 16; //fix zeby nie dalo sie zmniejszyc okna za bardzo
+            Width = targetWidth > sizeInfo.NewSize.Width ? targetWidth : sizeInfo.NewSize.Width;
+            mainGrid.Width = targetWidth;
+            mainGrid.RowDefinitions[1].Height = new GridLength(sizeInfo.NewSize.Height * mainGridHeightRatio);
         }
     }
 }
